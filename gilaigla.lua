ecf50adf2c6b50cd1dae8f6dcbd20397ec10cbde
d4e6f8a0b2c4d6e8f0a2c4e6f8a0b2c4d6 <eternalpro758@gmail.com>
2026-08-25 14:15:18 +0700

    Update gilaigla.lua

--- gilaigla.lua
+++ gilaigla.lua
@@ -14,48 +14,49 @@ if not queueteleport then
 
         queueteleport([=[
             game:GetService("ScriptContext"):SetTimeout(1)
-            local UserInputService = game:GetService("UserInputService")
-            local GuiService = game:GetService("GuiService")
-            local Platform = Enum.Platform.PS4
+            
+            -- Place ID check
+            if game.PlaceId == 10179538382 then
+                local UserInputService = game:GetService("UserInputService")
+                local GuiService = game:GetService("GuiService")
+                local Platform = Enum.Platform.PS4
 
-            local oldNamecall, oldIndex;
+                local oldNamecall, oldIndex;
 
-            oldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
-            if checkcaller() then
-                return oldNamecall(self, ...)
-             end
-    local method = getnamecallmethod()
-    if self == UserInputService or self == GuiService then
-        if method == "GetPlatform" then
-            return Platform
-        elseif method == "IsTenFootInterface" then
-            return true
-        elseif method == "GetPlatformName" then
-            return "PS4"
-        end
-    end
-    return oldNamecall(self, ...)
-end))
+                oldNamecall = hookmetamethod(game, "__namecall", function(self, ...)
+                    if checkcaller then
+                        return oldNamecall(self, ...)
+                    end
+                    local method = getnamecallmethod()
+                    if self == UserInputService or self == GuiService then
+                        if method == "GetPlatform" then
+                            return Platform
+                        elseif method == "IsTenFootInterface" then
+                            return true
+                        elseif method == "GetPlatformName" then
+                            return "PS4"
+                        end
+                    end
+                    return oldNamecall(self, ...)
+                end)
 
-oldIndex = hookmetamethod(game, "__index", newcclosure(function(self, index)
-    if checkcaller() then
-        return oldIndex(self, index)
-    end
-    if self == UserInputService and tostring(getcallingscript()) ~= "ControlModule" then
-        if index == "TouchEnabled"
-        or index == "MouseEnabled"
-        or index == "KeyboardEnabled" then
-            return false
-        elseif index == "GamepadEnabled"
-        or index == "ControllerEnabled" then
-            return true
-        end
-    end
-    return oldIndex(self, index)
-end))
+                oldIndex = hookmetamethod(game, "__index", function(self, index)
+                    if checkcaller then
+                        return oldIndex(self, index)
+                    end
+                    if self == UserInputService and tostring(getcallingscript()) ~= "ControlModule" then
+                        if index == "TouchEnabled" or index == "MouseEnabled" or index == "KeyboardEnabled" then
+                            return false
+                        elseif index == "GamepadEnabled" or index == "ControllerEnabled" then
+                            return true
+                        end
+                    end
+                    return oldIndex(self, index)
+                end)
 
-loadstring(game:HttpGet(""))()
-elseif game.PlaceId == 13643807539 then
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/d4e6f8a0b2c4d6e8f0a2c4e6f8a0b2c4d6/cloudflare-global/refs/heads/main/gilaigla.lua"))()
+                
+            elseif game.PlaceId == 13643807539 then
                 repeat task.wait() until not game.ReplicatedFirst:FindFirstChild("Intro")
                 game:GetService("ScriptContext"):SetTimeout(9e9)
             end
