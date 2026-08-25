32c0fe3b5155388c2a00296d20ab017e02f8a6d7
d4e6f8a0b2c4d6e8f0a2c4e6f8a0b2c4d6 <eternalpro758@gmail.com>
2026-08-25 20:40:47 +0700

    Update gilaigla.lua

--- gilaigla.lua
+++ gilaigla.lua
@@ -1,38 +1,48 @@
 queue_on_teleport([=[
 if game.PlaceId == 10179538382 then
-    local __OldNamecall = nil
-    __OldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
+    local UserInputService = game:GetService("UserInputService")
+    local GuiService = game:GetService("GuiService")
+
+    local OldNamecall
+    OldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
         local Method = getnamecallmethod()
 
-        if (Method == "IsTenFootInterface") then
-            if (debug.traceback():find("Intro")) then
-                return true
+        if Method == "IsTenFootInterface" then
+            return true
+        elseif Method == "GetPlatform" then
+            return Enum.Platform.XBoxOne
+        end
+
+        if hub_traceback and game.PlaceId == HUB_PLACE then
+            local Traceback = debug.traceback()
+            if Traceback:match("PlayerGui") then
+                local lp = game:GetService("Players").LocalPlayer
+                local SourceName = Traceback:gsub(
+                    string.format("Players.%s.PlayerGui.", lp.Name),
+                    ""
+                )
+                if SourceName:len() > 32 then
+                    return task.wait(9e9)
+                end
             end
         end
 
-        return __OldNamecall(self, ...)
+        return OldNamecall(self, ...)
     end))
 
-    repeat task.wait() until game:IsLoaded()
-
-    local Players = cloneref(game:GetService("Players")) or game:GetService("Players")
-    local GuiService = cloneref(game:GetService("GuiService")) or game:GetService("GuiService")
-    local RunService = cloneref(game:GetService("RunService")) or game:GetService("RunService")
-
-    local Client = Players.LocalPlayer
-
-    local OldIsTenFootInterface = nil
-    OldIsTenFootInterface = hookfunction(GuiService.IsTenFootInterface, newcclosure(function(self)
-        if not checkcaller() then
-            return true
-        end
-
-        return OldIsTenFootInterface(self)
-    end))
+    pcall(function()
+        local OldIsTenFootInterface
+        OldIsTenFootInterface = hookfunction(GuiService.IsTenFootInterface, newcclosure(function(self)
+            if not checkcaller() then
+                return true
+            end
+            return OldIsTenFootInterface(self)
+        end))
+    end)
 end
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/d4e6f8a0b2c4d6e8f0a2c4e6f8a0b2c4d6/cloudflare-global/refs/heads/main/gilaigla.lua"))()
-    
+
 elseif game.PlaceId == 13643807539 then
 repeat task.wait() until not game.ReplicatedFirst:FindFirstChild("Intro")
         game:GetService("ScriptContext"):SetTimeout(9e9)
